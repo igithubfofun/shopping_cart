4448a37bc45b18c82388d1c663c1c86c211a7c06
Metel Patel <metelpatel@gmail.com>
2016-04-27 15:09:30 -0500

    ipushfofun

---
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -0,0 +1,32 @@
+var AllItems = React.createClass({
+  getInitialState() {
+    return {items: [] }
+  },
+  componentDidMount() {
+    $.getJSON('/api/v1/items.json', (response) => {
+      this.setState({items: response})
+    }),
+    console.log('Component mounted');
+
+  },
+
+  render() {
+    var items = this.state.items.map((item) => {
+      return (
+        <div>
+          <h3>{item.name}</h3>
+          <p>{item.description}</p>
+        </div>
+        )
+    });
+
+    return (
+      <div>
+        {items}
+      </div>
+      )
+
+
+  }
+});
+
